f7020df0c26554061f0213fd231714be52e8ac8b
Anjali Raghav <anjali.raghav@gmail.com>
2018-06-20 10:20:39 +1200

    with tessearct code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import {Component} from '@angular/core';
+import * as Tesseract from 'tesseract.js';
+import * as fs from 'fs';
 
 @Component({
   selector: 'app-root',
@@ -7,7 +9,10 @@ import {Component} from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
-  links = [{
+  ocrReading: string;
+  loading: boolean;
+  fileName;
+  links: [{
     link: 'https://angular.io/tutorial',
     name: 'Angular IO Tutorial',
     icon: 'school'
@@ -20,4 +25,131 @@ export class AppComponent {
     name: 'Angular IO Blog',
     icon: 'info'
   }];
+
+
+  OnButtonClick() {
+    // const myImage = 'assets/z te atau-3-feb-69.03.png';
+    const myImage = 'assets/everest-24-jan-154.50.png';
+    // const myImage='assets/spark-november-bill-106.png';
+    // const myImage='assets/thai peninsula-2-nov-76.png';
+    // const myImage = 'assets/parking-16-feb-14.00.png';
+
+    // Step 1: Read files from folder
+    // not able to complete as fs library is not recognised with typescript. Need to find solution
+    /*   const testFolder = 'assets/';
+       console.log(fs);
+       fs.readdir(testFolder, (err, files) => {
+         files.forEach(file => {
+           console.log(file);
+         });
+       });*/
+
+    // Step 2: Tessearct OCR
+    Tesseract.recognize(myImage)
+      .progress(message => {
+        console.log(message);
+        this.loading = true;
+      })
+      .catch(err => console.error(err))
+      .then(result => {
+        console.log(result);
+
+        this.ocrReading = result.text;
+
+        console.log(result.lines);
+        const date = result.lines.filter(line => {
+            return line.text.includes('/') ? line.text.split(' ').filter(x => x.includes('/')) : '';
+          })
+        ;
+        const dateToAppend = date[0].text.split(' ').filter(x => x.includes('/'))[0].replace(/\//g, '_');
+
+        this.fileName = result.lines[1].text + '_' + dateToAppend + '.jpg';
+        this.loading = false;
+      })
+      .finally(resultOrError => console.log(resultOrError));
+
+    // Step 3: Once verified - email to given emailaddress for xero
+  }
+
+  GetTransactions(){
+
+  }
+
+
+  function getXeroClient(session) {
+    let config = {};
+    try {
+      config = require('./config/config.json');
+    } catch (ex) {
+      if (process && process.env && process.env.APPTYPE) {
+        //no config file found, so check the process.env.
+        config.appType = process.env.appType.toLowerCase();
+        config.callbackUrl = process.env.callbackUrl.toLowerCase();
+        config.consumerKey = process.env.consumerKey;
+        config.consumerSecret = process.env.consumerSecret;
+      } else {
+        throw "Config not found";
+      }
+    }
+
+    return new XeroClient(config, session);
+  }
+
+  async function authorizeRedirect(req, res, returnTo) {
+    var xeroClient = getXeroClient(req.session);
+    let requestToken = await xeroClient.oauth1Client.getRequestToken();
+
+    var authoriseUrl = xeroClient.oauth1Client.buildAuthoriseUrl(requestToken);
+    req.session.oauthRequestToken = requestToken;
+    req.session.returnTo = returnTo;
+    res.redirect(authoriseUrl);
+  }
+
+  function authorizedOperation(req, res, returnTo, callback) {
+    if (req.session.accessToken) {
+      callback(getXeroClient(req.session.accessToken));
+    } else {
+      authorizeRedirect(req, res, returnTo);
+    }
+  }
+
+  function handleErr(err, req, res, returnTo) {
+    console.log(err);
+    if (err.data && err.data.oauth_problem && err.data.oauth_problem == "token_rejected") {
+      authorizeRedirect(req, res, returnTo);
+    } else {
+      res.redirect('error', err);
+    }
+  }
+/*
+  app.get('/error', function(req, res) {
+    console.log(req.query.error);
+    res.render('index', { error: req.query.error });
+  })
+
+// Home Page
+  app.get('/', function(req, res) {
+    res.render('index', {
+      active: {
+        overview: true
+      }
+    });
+  });
+
+// Redirected from xero with oauth results
+  app.get('/access', async function(req, res) {
+    var xeroClient = getXeroClient();
+
+    let savedRequestToken = req.session.oauthRequestToken;
+    let oauth_verifier = req.query.oauth_verifier;
+    let accessToken = await xeroClient.oauth1Client.swapRequestTokenforAccessToken(savedRequestToken, oauth_verifier);
+
+    req.session.accessToken = accessToken;
+
+    var returnTo = req.session.returnTo;
+    res.redirect(returnTo || '/');
+  });*/
+
+
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {MatToolbarModule, MatButtonModule, MatIconModule, MatListModule, MatStepperModule} from '@angular/material';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {FlexLayoutModule} from '@angular/flex-layout';
 
@@ -8,6 +9,7 @@ import {FlexLayoutModule} from '@angular/flex-layout';
 import {AppComponent} from './app.component';
 import {DataComponent} from './data/data.component';
 import {DataService} from './data/shared/data.service';
+import { HttpClientModule } from '@angular/common/http';
 
 
 @NgModule({
@@ -23,7 +25,10 @@ import {DataService} from './data/shared/data.service';
     MatIconModule,
     MatStepperModule,
     BrowserAnimationsModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    MatProgressBarModule
+
+    HttpClientModule
   ],
   providers: [DataService], // name of the service required to pull data if any
   bootstrap: [AppComponent]  // name of the component bootstrapping
